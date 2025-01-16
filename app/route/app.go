8b2c26408e5a24d7b5e36523eda64efcb2fe3e60
zevinto <zevinto@163.com>
2025-01-16 11:13:12 +0800

    feat: 实现路由分组

---
+++ app/route/app.go
@@ -0,0 +1,9 @@
+package route
+
+import "github.com/gin-gonic/gin"
+
+func genAppRouter(rg *gin.RouterGroup) {
+	rg.GET("/b", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "b"})
+	})
+}
--- app/route/route.go
+++ app/route/route.go
@@ -1,21 +1,14 @@
 package route
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 type AppRouter struct{}
 
 func (*AppRouter) AddRoute(e *gin.Engine) {
-	e.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "ping"})
-	})
-
-	e.GET("/pong", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
+	genAdminRouter(e.Group("/admin"))
+	genAppRouter(e.Group("/app"))
 }
 
 func New() *AppRouter {
